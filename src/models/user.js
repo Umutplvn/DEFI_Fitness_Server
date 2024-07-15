1a0d88f54c1c7289d10f0304bb9fddf062fa4c9a
umutplvn <umutpehlivan2078@gmail.com>
2024-07-15 02:11:16 +0100

    password validate

--- src/models/user.js
+++ src/models/user.js
@@ -6,14 +6,10 @@
 const mongoose = require("mongoose");
 const passwordEncrypt = require("../helpers/passwordEncrypt");
 
+// Regex pattern for password validation
+const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])[a-zA-Z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
+
 const UserSchema = new mongoose.Schema(
-
-// {
-//   "email":"user@site.com",
-//   "password":"1234",
-//   "name":"user"    
-// }
-
   {
     email: {
       type: String,
@@ -22,7 +18,6 @@ const UserSchema = new mongoose.Schema(
       unique: true,
     },
 
-    
     name: {
       type: String,
       trim: true,
@@ -30,42 +25,46 @@ const UserSchema = new mongoose.Schema(
     },
 
     surname: {
-        type: String,
-        trim: true,
-      },
-  
-    dateOfBirth:{
-        type:Date,
-        trim:true,
+      type: String,
+      trim: true,
+    },
+
+    dateOfBirth: {
+      type: Date,
+      trim: true,
     },
 
     gender: {
-        type: String,
-        enum: ["Male", "Female"],
-      },
-  
-      password: {
-        type: String,
-        trim: true,
-        required: true,
-        set: (password) => passwordEncrypt(password),
-      },
-      
-      verified: {
-        type: Boolean,
-        default: false,
-      },
+      type: String,
+      enum: ["Male", "Female"],
+    },
 
-      isAdmin:{
-        type:Boolean,
-        default:false
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: (password) => passwordEncrypt(password),
+      validate: {
+        validator: (password) => passwordPattern.test(password),
+        message: (props) =>
+          `${props.value} is not a valid password format. Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.`,
       },
+    },
 
-      isPremium:{
-        type:Boolean,
-        default:false
-      },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
 
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+
+    isPremium: {
+      type: Boolean,
+      default: false,
+    },
 
     avatar: {
       type: String,
@@ -73,16 +72,14 @@ const UserSchema = new mongoose.Schema(
       default:
         "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.laptoppcapk.com%2Ffitness-point%2F&psig=AOvVaw0gWJzVmLitXNL91MZXqtOu&ust=1719788775218000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCU5sv2gYcDFQAAAAAdAAAAABAJ",
     },
-    sportBranch:[
+
+    sportBranch: [
       {
         type: String,
-      }
-    ]
-
+      },
+    ],
   },
   { timestamps: true, collection: "user" }
 );
 
-
 module.exports = mongoose.model("User", UserSchema);
-
