f17d853852b9b8bbe63aa9ec46a59e6b9c2ea7c1
umutplvn <umutpehlivan2078@gmail.com>
2024-07-28 23:56:53 +0100

    up

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,7 @@ const router = require("express").Router();
 const User = require("../controllers/user");
 const permissions =require('../middlewares/permissions')
 
-router.route("/").get(permissions.isAdmin, User.list)
+router.route("/").get(User.list)
 router.route("/register").post(User.create)
 router.route("/forgotpass").post(User.forgotPass)
 router.route("/updatepass").put(User.updatePassword)
