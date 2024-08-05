37b7c27d1c2519f78a75e5de0991146a837a8d83
umutplvn <umutpehlivan2078@gmail.com>
2024-08-05 03:32:43 +0100

    up

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -10,8 +10,8 @@ router.post('/create', blog.create)
 router.post('/like', permissions.isLogin, blog.like)
 router.get('/list', blog.list)
 router.get('/:blogId', blog.read)
-router.put('/update/:blogId',  blog.update)
-router.patch('/update/:blogId', blog.update)
+router.put('/update/:blogId', permissions.isAdmin, blog.update)
+router.patch('/update/:blogId', permissions.isAdmin, blog.update)
 router.delete('/delete/:blogId', permissions.isAdmin, blog.delete)
 
 
