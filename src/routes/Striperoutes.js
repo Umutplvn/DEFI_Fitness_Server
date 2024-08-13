0b44ffd69a9442ed6098d77e5f564f8d2be918e1
umutplvn <umutpehlivan2078@gmail.com>
2024-08-13 04:09:14 +0100

    up

--- src/routes/Striperoutes.js
+++ src/routes/Striperoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const stripeController = require('../controllers/stripeController'); 
 
+
 router.post('/create-checkout-session', stripeController.createCheckoutSession);
 router.post('/webhook', express.raw({ type: 'application/json' }), stripeController.webhook);
 
