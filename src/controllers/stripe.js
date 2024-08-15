112058fb6119fc8e9d8b0897ae43c1149a72c07b
umutplvn <umutpehlivan2078@gmail.com>
2024-08-15 23:39:29 +0100

    up

--- src/controllers/stripe.js
+++ src/controllers/stripe.js
@@ -35,7 +35,7 @@ const handleCheckoutSessionCompleted = async (event) => {
   const userId = session.metadata.userId;
 
   try {
-    const result = await User.updateOne({ _id: objectId }, { membership: 'Premium' });
+    const result = await User.updateOne({ _id: userId }, { membership: 'Premium' });
 
     if (result.modifiedCount > 0) {
       console.log(`User with ID: ${userId} updated to Premium`);
