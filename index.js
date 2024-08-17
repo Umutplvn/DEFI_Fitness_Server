58deee3c0ccc71b946de4067ed13597a3369c741
umutplvn <umutpehlivan2078@gmail.com>
2024-08-17 04:20:56 +0100

    up

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ app.use(require('cors')());
 
 app.post('/api/webhook', express.raw({ type: 'application/json' }), async (req, res) => {
     const sig = req.headers['stripe-signature'];
-    const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
-  
+    // const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+    const endpointSecret = "whsec_xizCMxLzS6gaRbiIkYJaoOPvdz49lQdu"
+    
     try {
       const event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
       
