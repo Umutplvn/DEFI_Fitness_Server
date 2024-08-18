0513d66c5edbaa4e07c5e9e276b63cdda897600c
umutplvn <umutpehlivan2078@gmail.com>
2024-08-18 04:27:37 +0100

    up

--- src/controllers/stripe.js
+++ src/controllers/stripe.js
@@ -1,25 +1,25 @@
 "use strict";
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const User =require('../models/user')
-const sendCancellationEmail =require('../helpers/cancellationNotification')
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const User = require("../models/user");
+const sendCancellationEmail = require("../helpers/cancellationNotification");
 
 const createCheckoutSession = async (req, res) => {
   const { priceId, userId, email } = req.body;
 
   if (!userId) {
-    return res.status(400).json({ error: 'User ID is required' });
+    return res.status(400).json({ error: "User ID is required" });
   }
 
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+      payment_method_types: ["card"],
       line_items: [{ price: priceId, quantity: 1 }],
-      mode: 'subscription',
+      mode: "subscription",
       success_url: `http://localhost:3000/profile`,
       cancel_url: `http://localhost:3000/profile`,
       customer_email: email,
-      metadata: { userId: userId }
+      metadata: { userId: userId },
     });
     res.json({ sessionId: session.id });
   } catch (error) {
@@ -27,37 +27,36 @@ const createCheckoutSession = async (req, res) => {
   }
 };
 
-
-
 const cancelSubscription = async (req, res) => {
-    const { subscriptionId } = req.params;
-    const { userId } = req.body;
-const user=await User.findOne({_id:userId})
-    try {
-        const deletedSubscription = await stripe.subscriptions.cancel(subscriptionId);
+  const { subscriptionId } = req.params;
+  const { userId } = req.body;
+  const user = await User.findOne({ _id: userId });
+  try {
+    const deletedSubscription = await stripe.subscriptions.cancel(
+      subscriptionId
+    );
 
-        const result = await User.updateOne(
-            { _id: userId },
-            { membership: 'Basic' },
-            { new: true, runValidators: true }
-        );
+    const result = await User.updateOne(
+      { _id: userId },
+      { membership: "Basic" },
+      { new: true, runValidators: true }
+    );
 
-        sendCancellationEmail(user.email)
-        if (result) {
-            console.log(`User with ID: ${userId} updated to Basic`);
-
-        } else {
-            console.error(`User with ID: ${userId} was not updated.`);
-        }
-
-        res.status(200).json(deletedSubscription);
-    } catch (error) {
-        console.error('Failed to cancel subscription:', error);
-        res.status(500).json({ error: error.message });
+    sendCancellationEmail(user.email);
+    if (result) {
+      console.log(`User with ID: ${userId} updated to Basic`);
+    } else {
+      console.error(`User with ID: ${userId} was not updated.`);
     }
+
+    res.status(200).json(deletedSubscription);
+  } catch (error) {
+    console.error("Failed to cancel subscription:", error);
+    res.status(500).json({ error: error.message });
+  }
 };
-  
+
 module.exports = {
   createCheckoutSession,
-  cancelSubscription
+  cancelSubscription,
 };
