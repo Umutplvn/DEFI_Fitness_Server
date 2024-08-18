698c798bbfc5d67af133bfebe8c41ed0d8c2b146
umutplvn <umutpehlivan2078@gmail.com>
2024-08-18 05:30:30 +0100

    up

--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -5,9 +5,6 @@
 
 const mongoose = require('mongoose')
 
-// const MONGODB = process.env.MONGODB || 'mongodb://localhost:27017/defi'
-// mongoose.connect(MONGODB)
-
 mongoose.connect(process.env.MONGODB)
     .then(() => console.log(' * DB Connected * '))
     .catch((err) => console.log(' * DB Not Connected * ', err))
